d434317b52ceaa30b5b29fc6cabb466b309bcc96
volnova <nastya.volnova@gmail.com>
2018-01-09 18:15:26 +0700

    add procfile

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from models import Profile
+from home.models import Profile
 from django import forms
 
 
