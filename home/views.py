a34624363e765d409aa69db597cfad9652fa2e09
volnova <nastya.volnova@gmail.com>
2018-01-09 17:30:56 +0700

    first commit

---
+++ home/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.db import transaction
+from .models import Profile
+from .forms import UserForm,ProfileForm
+
+
+@login_required
+def Home(request):
+    return render(request, 'home/home.html')
+
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect('/')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'home/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
+
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
